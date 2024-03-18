4afc07e52ba841729b0b276d91fac7cbdc9b298b
G K <gk@setup.com>
2024-03-18 13:21:11 -0400

    Rmv console

--- src/maplibre-gl-measures.js
+++ src/maplibre-gl-measures.js
@@ -538,7 +538,6 @@ export default class MeasuresControl {
   _handleOnRender() {
     if (this.options && this.options.onRender !== null && this.options.onRender !== undefined) {
       const features = this._getDrawnFeatures();
-      console.log(features);
       // Return if no features drawn
       if (!features || !features.features || features.features == undefined || features.features.length < 1) {
         return;
